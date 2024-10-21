fbab690614d6c7278d54341261e77bd63069afc1
Antonio Silva <antoniofranciscosilva.sp@gmail.com>
2024-10-21 20:51:57 -0300

    Implementando Controller, Model e Service Person

---
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model;
+using RestWithASPNET.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]    
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {            
+            var person = _personService.FindByID(id);
+
+            // Nesse momento como esta Mock nunca vai retornar null, mas quando implementar BD pode retornar
+            if (person == null) return NotFound();
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            // Nesse momento como esta Mock nunca vai retornar null, mas quando implementar BD pode retornar
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            // Nesse momento como esta Mock nunca vai retornar null, mas quando implementar BD pode retornar
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/Services/Implementation/PersonServiceImplementation.cs
@@ -0,0 +1,73 @@
+﻿using RestWithASPNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Services.Implementation
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            // Implementar o acesso a BD para incluir
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            // Implementar o acesso a BD para incluir
+        }
+
+        public List<Person> FindAll()
+        {
+            //Por enquanto Mock
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindByID(long id)
+        {
+            //Por enquanto Mock
+            return new Person
+            {
+                Id = IncrementAndGet(),,
+                FirstName = "Antonio",
+                LastName = "Silva",
+                Address = "Guarulhos - SP, Brazil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            // Implementar o acesso a BD para alterar
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + 1,
+                LastName = "Person LastName " + 1,
+                Address = "Aome Address " + 1,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
